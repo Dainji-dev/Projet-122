4a8b0fb420ad7b4c38e18713b5831993aedaf24e
Dainji <perez.romain09.code@gmail.com>
2026-06-11 15:24:52 +0200

    ajout de creation_utilisateur_test.sh

--- scripts/creation_utilisateur_test.sh
+++ scripts/creation_utilisateur_test.sh
@@ -72,25 +72,25 @@ touch log.txt
 
 
 # Créer l'arborescence dans Nom_Prenom
-if mkdir "$userFolder" 2>> errors.txt; then
+if echo "Création du dossier $userFolder" 2>> errors.txt; then
     echo "[$todayDate] Dossier $userFolder créé avec succès" >> log.txt
 else
     echo "[$todayDate] Erreur lors de la création du dossier $userFolder" >> errors.txt
 fi
 
-if mkdir "$userFolder/Documents" 2>> errors.txt; then
+if echo "Création du dossier $userFolder/Documents" 2>> errors.txt; then
     echo "[$todayDate] Dossier $userFolder/Documents créé avec succès" >> log.txt
 else
     echo "[$todayDate] Erreur lors de la création du dossier $userFolder/Documents" >> errors.txt
 fi
 
-if mkdir "$userFolder/Projets" 2>> errors.txt; then
+if echo "Création du dossier $userFolder/Projets" 2>> errors.txt; then
     echo "[$todayDate] Dossier $userFolder/Projets créé avec succès" >> log.txt
 else
     echo "[$todayDate] Erreur lors de la création du dossier $userFolder/Projets" >> errors.txt
 fi
 
-if mkdir "$userFolder/Archives" 2>> errors.txt; then
+if echo "Création du dossier $userFolder/Archives" 2>> errors.txt; then
     echo "[$todayDate] Dossier $userFolder/Archives créé avec succès" >> log.txt
 else
     echo "[$todayDate] Erreur lors de la création du dossier $userFolder/Archives" >> errors.txt
@@ -98,7 +98,7 @@ fi
 
 
 # Créer le fichier bienvenue.txt s'il n'existe pas
-if touch "$userFolder/bienvenue.txt" 2>> errors.txt; then
+if echo "Création du fichier $userFolder/bienvenue.txt" 2>> errors.txt; then
     echo "[$todayDate] Fichier $userFolder/bienvenue.txt créé avec succès" >> log.txt
 else
     echo "[$todayDate] Erreur lors de la création du fichier $userFolder/bienvenue.txt" >> errors.txt
@@ -106,35 +106,19 @@ fi
 
 
 # écrire le texte dans bienvenue.txt 
-if echo "Bonjour $firstName $lastName," >> "$userFolder/bienvenue.txt" 2>> errors.txt; then
-    echo "[$todayDate] La ligne correctement écrite dans $userFolder/bienvenue.txt" >> log.txt
-else
-    echo "[$todayDate] La ligne n'as pas été écrite dans $userFolder/bienvenue.txt" >> errors.txt
-fi
-
-if echo "Bienvenue dans l'entreprise" >> "$userFolder/bienvenue.txt" 2>> errors.txt; then
-    echo "[$todayDate] La ligne correctement écrite dans $userFolder/bienvenue.txt" >> log.txt
-else
-    echo "[$todayDate] La ligne n'as pas été écrite dans $userFolder/bienvenue.txt" >> errors.txt
-fi
-
-if echo "Ce dossier à été créé automatiquement le $todayDate" >> "$userFolder/bienvenue.txt" 2>> errors.txt; then
-    echo "[$todayDate] La ligne correctement écrite dans $userFolder/bienvenue.txt" >> log.txt
-else
-    echo "[$todayDate] La ligne n'as pas été écrite dans $userFolder/bienvenue.txt" >> errors.txt
-fi
+echo "écriture du texte de bienvenue dans le dossier $userFolder/bienvenue.txt"
 
 # Copie de la charte et de la checklist
 
 # ---------- Charte.pdf ----------
-if cp "$charte" "$userFolder/Documents" 2>> errors.txt; then
+if echo "Copie de $charte vers $userFolder/Documents" 2>> errors.txt; then
     echo "[$todayDate] Fichier $charte copié avec succès" >> log.txt
 else
     echo "[$todayDate] Erreur lors de la copie du fichier $charte" >> errors.txt
 fi
 
 # ---------- Checklist.xlsx ----------
-if cp "$checklist" "$userFolder/Documents" 2>> errors.txt; then
+if echo "Copie de $checklist vers $userFolder/Documents" 2>> errors.txt; then
     echo "[$todayDate] Fichier $checklist copié avec succès" >> log.txt
 else
     echo "[$todayDate] Erreur lors de la copie du fichier $checklist" >> errors.txt
